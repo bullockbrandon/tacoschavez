dcba05e1277605ccba1e168083a4fd25865ac6f0
bullockbrandon <bullock.brandon@mcm.edu>
2024-10-09 08:50:52 -0500

    updated files

---
+++ content/js/avia-footer-scripts.js
@@ -0,0 +1,6 @@
+/*!
+ * Avia Footer Scripts
+ * Dynamic scripts for Avia Framework
+ * License: GPL
+ */
+!function(a,b){"object"==typeof module&&"object"==typeof module.exports?module.exports=b():b()}(this,function(){return function(a){function b(d){if(c[d])return c[d].exports;var e=c[d]={exports:{},id:d,loaded:!1};return a[d].call(e.exports,e,e.exports,b),e.loaded=!0,e.exports}var c={};return b.m=a,b.c=c,b.p="",b(0)}([function(a,b){"use strict";Object.defineProperty(b,"__esModule",{value:!0}),b.footerScriptVersion="2524ebd77a50573d7a0f1e5e5750455f---669fcf7a90314"}])});
