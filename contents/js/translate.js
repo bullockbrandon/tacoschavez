db78cca6822cbbf3d6273ff5e1de6d4462fd143c
bullockbrandon <bullock.brandon@mcm.edu>
2024-11-19 13:23:45 -0600

    Update translate.js

--- contents/js/translate.js
+++ contents/js/translate.js
@@ -127,7 +127,7 @@
     else
         widget_code += '<div id="gt_float_wrapper" style="position:fixed;'+switcher_vertical_position+':20px;'+switcher_horizontal_position+':20px;z-index:999999;"><div class="gt_float_switcher notranslate" style="opacity:0">';
 
-    var gt_current_div = '<div class="gt-selected"><div class="gt-current-lang"><img src="" alt="'+current_lang+'"> <span class="gt-lang-code">'+current_lang+'</span> <span class="gt_float_switcher-arrow"></span></div></div>';
+    var gt_current_div = '<div class="gt-selected"><div class="gt-current-lang"><img src="'+current_lang+'" alt="'+current_lang+'"> <span class="gt-lang-code">'+current_lang+'</span> <span class="gt_float_switcher-arrow"></span></div></div>';
     var gt_options_div = '<div class="gt_options" style="display:none">';
     languages.forEach(function(lang) {
         var el_a = document.createElement('a');
@@ -137,7 +137,7 @@
         el_a.setAttribute('data-gt-lang', lang);
 
         var el_img = document.createElement('img');
-        el_img.setAttribute('data-gt-lazy-src', get_flag_src(lang));
+        el_img.setAttribute('data-gt-lazy-src', lang);
         el_img.alt = lang;
 
         el_a.appendChild(el_img);
